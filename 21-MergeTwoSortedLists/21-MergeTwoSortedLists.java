3c3203a6806a56ff3d8b43c8bbf5370588a3eea1
Elizabeth O'Callaghan <elizabethmocallaghan@gmail.com>
2025-11-27 21:30:03 -0500

    Create 21-MergeTwoSortedLists

---
+++ 21-MergeTwoSortedLists/21-MergeTwoSortedLists.java
@@ -0,0 +1,31 @@
+// Last updated: 11/27/2025, 9:30:03 PM
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode merged = new ListNode();
+        ListNode current = merged;
+        while(list1 != null && list2 != null){
+            if(list1.val<list2.val){
+                current.next = list1;
+                list1 = list1.next;
+            }
+            else{
+                current.next = list2;
+                list2 = list2.next;
+            }
+            current = current.next;
+        }
+
+        current.next = (list1 != null) ? list1 : list2;
+        return merged.next;
+    }
+}
