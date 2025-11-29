8919bd00052d4bfd9f2177d9f0c5b5fe3d5d8580
Elizabeth O'Callaghan <elizabethmocallaghan@gmail.com>
2025-11-29 12:49:25 -0500

    File updated at 11/29/2025, 12:49:25 PM

--- 4-MedianofTwoSortedArrays/4-MedianofTwoSortedArrays.java
+++ 4-MedianofTwoSortedArrays/4-MedianofTwoSortedArrays.java
@@ -1,30 +1,43 @@
-// Last updated: 11/29/2025, 12:23:42 PM
+// Last updated: 11/29/2025, 12:49:25 PM
 1class Solution {
-2    public String convert(String s, int numRows) {
-3        if(numRows == 1 || numRows>=s.length()){
-4            return s;
+2    public int reverse(int x) {
+3        if(x == 0){
+4            return x;
 5        }
-6        int index = 0, d=1;
-7        List<Character>[] rows = new ArrayList[numRows];
-8        for(int i=0; i < numRows; i++){
-9            rows[i] = new ArrayList<>();
-10        }
-11
-12        for(char c: s.toCharArray()){
-13            rows[index].add(c);
-14            if(index == 0){
-15                d = 1;
-16            } else if(index == numRows - 1){
-17                d = -1;
-18            }
-19            index += d;
-20        }
-21        StringBuilder result = new StringBuilder();
-22        for(List<Character> row : rows){
-23            for(char c: row){
-24                result.append(c);
-25            }
-26        }
-27        return result.toString();
-28    }
-29}
+6        int negativeModifier = x<0 ? -1 : 1;
+7        x = x*negativeModifier;
+8        //remove any trailing zeros from end;
+9        while(x%10 == 0){
+10            x=x/10;
+11        }
+12        if(countDigits(x) == 10){
+13            StringBuilder reversedStr = new StringBuilder(Integer.toString(x)).reverse();
+14            StringBuilder maxInt = new StringBuilder(Integer.toString(Integer.MAX_VALUE));
+15            for(int i=0; i< reversedStr.length(); i++){
+16                int reversedDigit = reversedStr.charAt(i) - 0;
+17                int maxIntDigit = maxInt.charAt(i) - 0;
+18                if(reversedDigit < maxIntDigit){
+19                    break;
+20                } else if(reversedDigit > maxIntDigit){
+21                    return 0;
+22                }
+23            }
+24        }
+25
+26        int flipped = 0;
+27        while(x>0){
+28            flipped = flipped * 10 + x%10;
+29            x = x/10;
+30        }
+31        return negativeModifier * flipped;
+32    }
+33
+34    private int countDigits(int x){
+35        int digits = 0;
+36        while(x>0){
+37            x = x/10;
+38            digits++;
+39        }
+40        return digits;
+41    }
+42}
