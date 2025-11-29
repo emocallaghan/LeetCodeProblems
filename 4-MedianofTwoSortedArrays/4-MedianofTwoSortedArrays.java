4a9611a2e7e8096a9a3f0c7709faafb85279c2e5
Elizabeth O'Callaghan <elizabethmocallaghan@gmail.com>
2025-11-29 12:23:42 -0500

    File updated at 11/29/2025, 12:23:42 PM

--- 4-MedianofTwoSortedArrays/4-MedianofTwoSortedArrays.java
+++ 4-MedianofTwoSortedArrays/4-MedianofTwoSortedArrays.java
@@ -1,29 +1,30 @@
-// Last updated: 11/27/2025, 11:04:52 PM
+// Last updated: 11/29/2025, 12:23:42 PM
 1class Solution {
-2    public String longestPalindrome(String s) {
-3        if(s.length()<=1){
+2    public String convert(String s, int numRows) {
+3        if(numRows == 1 || numRows>=s.length()){
 4            return s;
 5        }
-6        String longest = s.substring(0,1);
-7        for(int i=0; i<s.length()-1; i++){
-8            String oddPalindrome = getPalindrome(s, i, i);
-9            String evenPalindrome = getPalindrome(s, i, i+1);
-10            if(oddPalindrome.length()>longest.length()){
-11                longest = oddPalindrome;
-12            }
-13            if(evenPalindrome.length()>longest.length()){
-14                longest = evenPalindrome;
-15            }
-16        }
-17        return longest;
-18        
-19    }
-20
-21    private String getPalindrome(String s, int left, int right){
-22        while(left >= 0 && right<s.length() && s.charAt(left)== s.charAt(right)){
-23            left --;
-24            right ++;
-25        }
-26        return s.substring(left+1, right);
-27    }
-28}
+6        int index = 0, d=1;
+7        List<Character>[] rows = new ArrayList[numRows];
+8        for(int i=0; i < numRows; i++){
+9            rows[i] = new ArrayList<>();
+10        }
+11
+12        for(char c: s.toCharArray()){
+13            rows[index].add(c);
+14            if(index == 0){
+15                d = 1;
+16            } else if(index == numRows - 1){
+17                d = -1;
+18            }
+19            index += d;
+20        }
+21        StringBuilder result = new StringBuilder();
+22        for(List<Character> row : rows){
+23            for(char c: row){
+24                result.append(c);
+25            }
+26        }
+27        return result.toString();
+28    }
+29}
