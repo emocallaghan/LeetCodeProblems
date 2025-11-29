6d08039af49f5728bdc496fb6b30343e1fa108b1
Elizabeth O'Callaghan <elizabethmocallaghan@gmail.com>
2025-11-29 13:50:50 -0500

    File updated at 11/29/2025, 1:50:49 PM

--- 4-MedianofTwoSortedArrays/4-MedianofTwoSortedArrays.java
+++ 4-MedianofTwoSortedArrays/4-MedianofTwoSortedArrays.java
@@ -1,43 +1,47 @@
-// Last updated: 11/29/2025, 12:49:25 PM
+// Last updated: 11/29/2025, 1:50:49 PM
 1class Solution {
-2    public int reverse(int x) {
-3        if(x == 0){
-4            return x;
+2    public int myAtoi(String s) {
+3        if(s==null){
+4            return 0;
 5        }
-6        int negativeModifier = x<0 ? -1 : 1;
-7        x = x*negativeModifier;
-8        //remove any trailing zeros from end;
-9        while(x%10 == 0){
-10            x=x/10;
-11        }
-12        if(countDigits(x) == 10){
-13            StringBuilder reversedStr = new StringBuilder(Integer.toString(x)).reverse();
-14            StringBuilder maxInt = new StringBuilder(Integer.toString(Integer.MAX_VALUE));
-15            for(int i=0; i< reversedStr.length(); i++){
-16                int reversedDigit = reversedStr.charAt(i) - 0;
-17                int maxIntDigit = maxInt.charAt(i) - 0;
-18                if(reversedDigit < maxIntDigit){
-19                    break;
-20                } else if(reversedDigit > maxIntDigit){
-21                    return 0;
-22                }
-23            }
-24        }
-25
-26        int flipped = 0;
-27        while(x>0){
-28            flipped = flipped * 10 + x%10;
-29            x = x/10;
-30        }
-31        return negativeModifier * flipped;
-32    }
-33
-34    private int countDigits(int x){
-35        int digits = 0;
-36        while(x>0){
-37            x = x/10;
-38            digits++;
-39        }
-40        return digits;
-41    }
-42}
+6        s = s.trim();
+7        if(s.equals("") || s.equals("-") || s.equals("+")){
+8            return 0;
+9        }
+10        StringBuilder sb = new StringBuilder(s);
+11        int negativeModifier = 1;
+12        int converted = 0;
+13        int numDigits = 0;
+14        if(sb.charAt(0) == '-' || sb.charAt(0)== '+'){
+15            negativeModifier = sb.charAt(0) == '-' ? -1 : 1;
+16            sb.deleteCharAt(0);
+17        }
+18        for(int i=0; i < sb.length(); i++){
+19            int nextDigit = sb.charAt(i) - '0';
+20            if(nextDigit>=0 && nextDigit<=9){
+21                if(numDigits<9){
+22                    converted = converted*10 + nextDigit;
+23                    if(converted > 0){
+24                        numDigits++;
+25                    }
+26                }
+27                else if(converted >= Integer.MAX_VALUE/10){
+28                    if(converted > Integer.MAX_VALUE/10 || 
+29                    (converted == Integer.MAX_VALUE/10 && nextDigit > Integer.MAX_VALUE%10)){
+30                        return negativeModifier == 1 ? Integer.MAX_VALUE : negativeModifier*Integer.MAX_VALUE-1;
+31                    }
+32                    converted = converted*10 + nextDigit;
+33                    numDigits++;
+34                }
+35                else{
+36                    converted = converted*10 + nextDigit;
+37                    numDigits++; 
+38                }
+39            }
+40            else{
+41                break;
+42            }
+43        }
+44        return negativeModifier * converted;
+45    }
+46}
